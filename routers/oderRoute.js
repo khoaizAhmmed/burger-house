7c1adb4f872b238a58d82624bac8d83cfc3d8f97
khoaiz ahmmed <khoaizahmedctg@gmail.com>
2021-07-17 12:10:15 +0600

    orderRoute update

--- routers/oderRoute.js
+++ routers/oderRoute.js
@@ -4,6 +4,6 @@ const { requireAuth } = require('../middleware/authmiddleware')
 const decorateHtmlResponse = require('../middleware/decorateHtmlResponse');
 
 router.get('/order', decorateHtmlResponse('Order'), requireAuth, OrderController.oderNow)
-router.post('/order-purchase', decorateHtmlResponse('purchase'), requireAuth, OrderController.orderPurchase)
+router.post('/order-purchase', decorateHtmlResponse('purchase'), OrderController.orderPurchase)
 
 module.exports = router
